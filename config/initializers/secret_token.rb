8f2928f8d4549a237bd1af6377c5d83532e8c32f
codystringham <cs.negativ@gmail.com>
2014-03-08 14:35:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stringy::Application.config.secret_token = '8fd643d99c99d3d34c84787586ea0cb2ae607084b9015f0268874dfb9b6519dace6d7e84460adee129e6fc86533aed6604d6f7cc3118e078bb5a2b6634556691'
