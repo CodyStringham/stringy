d350c48e1dd5fecb6f849eca8a047c699296d80c
codystringham <cs.negativ@gmail.com>
2016-02-07 13:46:21 -0700

    helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def draw_svg(id_name, svg_name)
-    content_tag :object, id: id_name, data: asset_url(svg_name), type: "image/svg+xml" do
+    content_tag :object, id: id_name, data: "/assets#{asset_path(svg_name)}", type: "image/svg+xml" do
       javascript_tag("new Vivus('#{id_name}', {duration: 200});")
     end.html_safe
   end
