7380e36ce588d69ad0b3794622f450ad6ae8f693
codystringham <cs.negativ@gmail.com>
2016-02-07 23:38:17 -0700

    finishes animations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-  def draw_svg(id_name, svg_name)
+  def draw_svg(id_name, svg_name, speed = 200)
     content_tag :object, id: id_name, data: asset_path("#{svg_name}.svg"), type: "image/svg+xml" do
-      javascript_tag("new Vivus('#{id_name}', {duration: 200});")
+      javascript_tag("new Vivus('#{id_name}', {duration: #{speed.to_i}});")
     end.html_safe
   end
 
